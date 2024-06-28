b16a9d52b6f763df5993b1bb156693bf6046f5c6
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-06-28 21:43:27 +0530

    Time: 20 ms (43.88%) | Memory: 11.5 MB (98.48%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -5,10 +5,11 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        count_s, count_t = Counter(s), Counter(t)
-        for c in count_t:
-            if c not in count_s:
-                return c
-            if count_s[c] < count_t[c]:
-                return c    
+        sum_s, sum_t = 0, 0
+        for c in s:
+            sum_s += ord(c)
+        for c in t:
+            sum_t += ord(c)
+        return chr(sum_t-sum_s)
+
         
