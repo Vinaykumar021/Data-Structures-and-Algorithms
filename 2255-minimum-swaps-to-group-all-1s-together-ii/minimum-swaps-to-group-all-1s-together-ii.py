46844bb56bd142fac685939e99766db97283a138
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-08-03 12:44:18 +0530

    Time: 795 ms (13.65%) | Memory: 20.7 MB (38.26%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -4,14 +4,12 @@ class Solution:
         l = 0
         window_ones = max_window_ones = 0
         total_ones = nums.count(1)
-
-        for r in range(2*n):
+        
+        for r in range(2 * n):
             if nums[r % n]:
                 window_ones += 1
             if r - l + 1 > total_ones:
                 window_ones -= nums[l % n]
-                l += 1 
+                l += 1
             max_window_ones = max(window_ones, max_window_ones)
         return total_ones - max_window_ones
-
-        
