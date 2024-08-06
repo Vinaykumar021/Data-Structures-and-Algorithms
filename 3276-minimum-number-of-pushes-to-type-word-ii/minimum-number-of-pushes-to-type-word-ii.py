6a8a57f2a777c3271e0c93b6e48127f3e9175e9d
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-08-07 00:16:20 +0530

    Time: 142 ms (85.44%) | Memory: 17.8 MB (8.11%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
         chr = sorted(Counter(word).values(), reverse = True)
-        return (sum(chr[:8]) + 2 * sum(chr[8:16]) + 3 * sum(chr[16:24]) + 4 * sum(chr[24:]))
+
+        return (sum(chr[:8]) + 2*sum(chr[8:16]) + 3*sum(chr[16:24]) + 4*sum(chr[24:]))
         
