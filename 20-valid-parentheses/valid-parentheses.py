cb0e3eb9d52055db57378e9062175a4ea3bb0589
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-08-10 23:57:53 +0530

    Time: 42 ms (15.37%) | Memory: 16.5 MB (87.73%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {"]" : "[", ")" : "(", "}" : "{"}
+        stack = []
+
+        for c in s:
+            if c not in map:
+                stack.append(c)
+                continue
+            elif not stack or stack[-1] != map[c]:
+                return False
+            stack.pop()
+
+
+        return not stack
+        
