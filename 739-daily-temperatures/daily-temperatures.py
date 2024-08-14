007529fa8e187d27dbb3911983097ada4282ba1b
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-08-15 00:40:28 +0530

    Time: 871 ms (79.07%) | Memory: 31.7 MB (26.49%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -4,10 +4,9 @@ class Solution:
         res = [0] * len(temperatures)
 
         for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
+            while stack and t > stack[-1][0]: # stack[-1][0] top element first index, stack[0][-1] is bottom or last element last index
                 stackTemp, stackInd = stack.pop()
                 res[stackInd] = (i - stackInd)
             stack.append((t, i))
         return res
-
         
