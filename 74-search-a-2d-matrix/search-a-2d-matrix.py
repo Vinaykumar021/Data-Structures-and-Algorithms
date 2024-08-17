6b08d7af2539f8e88adfa9eaf83e706d12e04e94
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-08-17 23:18:40 +0530

    Time: 44 ms (68.81%) | Memory: 17.1 MB (19.57%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -7,9 +7,11 @@ class Solution:
         while row < len(matrix) and col >= 0:
             if matrix[row][col] == target:
                 return True
-            elif matrix[row][col] < target:
-                row += 1
             elif matrix[row][col] > target:
                 col -= 1
+            elif matrix[row][col] < target:
+                row += 1
         return False
+
+        
         
