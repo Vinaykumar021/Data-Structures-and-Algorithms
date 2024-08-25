5ae5c58b15178cdcd123c2dfd15d9aba4bf4de0c
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-08-26 00:16:34 +0530

    Time: 41 ms (36.66%) | Memory: 16.4 MB (97.41%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -7,9 +7,8 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         tail = dummy
-
         while list1 and list2:
-            if list1.val < list2.val:
+            if list1.val <= list2.val:
                 tail.next = list1
                 list1 = list1.next
             else:
@@ -18,7 +17,6 @@ class Solution:
             tail = tail.next
         if list1:
             tail.next = list1
-        else:
+        elif list2:
             tail.next = list2
-
         return dummy.next
