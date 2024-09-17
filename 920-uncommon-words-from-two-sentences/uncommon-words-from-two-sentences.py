c9c551d08e2032eb8fd0472ba50833d5437eba86
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-09-17 23:01:18 +0530

    Time: 27 ms (95.43%) | Memory: 16.4 MB (96.74%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
@@ -3,6 +3,4 @@ class Solution:
         count = defaultdict(int)
         for w in s1.split() + s2.split():
             count[w] += 1
-        
-
         return [w for w, cnt in count.items() if cnt == 1]
