19a7a33478b547043b32cdcf602b929111c096b4
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-09-27 00:36:46 +0530

    Time: 75 ms (17.28%) | Memory: 16.8 MB (8.77%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -4,17 +4,18 @@ class Solution:
         candidates.sort()
 
         def dfs(i, cur, total):
-            if total == target:
-                return res.append(cur.copy())
-            if total > target or i >= len(candidates):
+            if target == total:
+                res.append(cur[::])
                 return
-            
+            if total > target or i >= len(candidates):
+                return 
             cur.append(candidates[i])
+
             dfs(i+1, cur, total + candidates[i])
             cur.pop()
 
             while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
-                 i += 1
+                i += 1
             dfs(i+1, cur, total)
-        dfs(0,[], 0)
-        return res 
+        dfs(0, [], 0)
+        return res
