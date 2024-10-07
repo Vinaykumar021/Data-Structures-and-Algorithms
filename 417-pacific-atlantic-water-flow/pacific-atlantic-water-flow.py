8a3e551f756ca21b9768d4406ea633b21ef386c1
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-10-08 02:02:49 +0530

    Time: 239 ms (18.07%) | Memory: 18.3 MB (37.30%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -4,25 +4,23 @@ class Solution:
         pac, atl = set(), set()
 
         def dfs(r, c, visit, prevHeight):
-            if (
-                (r, c) in visit or
-                r not in range(ROWS)
-                or c not in range(COLS)
-                or heights[r][c] < prevHeight
-            ):
+            if (r not in range(ROWS)
+            or c not in range(COLS)
+            or (r, c) in visit
+            or heights[r][c] < prevHeight):
                 return
             visit.add((r, c))
             dfs(r + 1, c, visit, heights[r][c])
             dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
             dfs(r, c - 1, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
 
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
             dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
         
         res = []
         for r in range(ROWS):
@@ -31,4 +29,4 @@ class Solution:
                     res.append([r, c])
         return res
 
-
+        
