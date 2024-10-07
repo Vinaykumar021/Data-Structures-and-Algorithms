b12ab391370f64d525381b2c09a07ef87bd7ef06
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-10-08 01:47:11 +0530

    Time: 232 ms (22.75%) | Memory: 18.3 MB (37.30%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -5,30 +5,30 @@ class Solution:
 
         def dfs(r, c, visit, prevHeight):
             if (
-                (r, c) in visit
-                or r not in range(ROWS)
+                (r, c) in visit or
+                r not in range(ROWS)
                 or c not in range(COLS)
                 or heights[r][c] < prevHeight
             ):
                 return
-
             visit.add((r, c))
             dfs(r + 1, c, visit, heights[r][c])
             dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])                
+            dfs(r, c + 1, visit, heights[r][c])
             dfs(r, c - 1, visit, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS - 1, atl, heights[r][COLS - 1])
-
-        res = []
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
         
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) in pac and (r, c) in atl:
                     res.append([r, c])
         return res
+
+
