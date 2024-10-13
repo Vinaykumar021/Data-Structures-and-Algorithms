68e3a062f0e9addbc48a4acbcacebbdba8952890
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-10-14 00:49:13 +0530

    Time: 34 ms (73.80%) | Memory: 16.6 MB (5.56%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,10 +1,15 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        reverseDigits = digits[::-1]
-        for i in range(len(reverseDigits)):
-            if reverseDigits[i] == 9:
-                reverseDigits[i] = 0
+        digits = digits[::-1]
+        carry = 1
+        i = 0
+        while carry and i < len(digits):
+            if digits[i] == 9:
+                digits[i] = 0
+                
             else:
-                reverseDigits[i] += 1
-                return reverseDigits[::-1]
-        return [1] + reverseDigits
+                digits[i] += 1
+                carry = 0
+                return digits[::-1]
+            i += 1
+        return [1] + digits
