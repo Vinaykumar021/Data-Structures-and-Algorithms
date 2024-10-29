15f6e106086fef9ffb77c9678699cc9435625ee6
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-10-30 02:23:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (13.12%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -8,8 +8,9 @@ class Solution:
             if s[i] == '0':
                 return 0
             res = dfs(i + 1)
-            if (i + 1 < len(s) and (s[i] == '1' or (s[i] == '2') and s[i + 1] in '0123456')):
+            if (i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in '0123456'))):
                 res += dfs(i + 2)
             dp[i] = res
             return res
         return dfs(0)
+        
