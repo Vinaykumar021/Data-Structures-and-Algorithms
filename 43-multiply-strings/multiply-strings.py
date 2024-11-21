58bbbb3051bf84ca317fc9d10bb61e68dec65898
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-11-22 00:11:26 +0530

    Time: 63 ms (29.92%) | Memory: 16.7 MB (19.87%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -2,8 +2,9 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if "0" in [num1, num2]:
             return "0"
+        
         res = [0] * (len(num1) + len(num2))
-        num1, num2 = num1[::-1],  num2[::-1]
+        num1, num2 = num1[::-1], num2[::-1]
         for i in range(len(num1)):
             for j in range(len(num2)):
                 digit = int(num1[i]) * int(num2[j])
