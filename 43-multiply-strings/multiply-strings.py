c75d15a92dbfb6cea5c443bdc79489863a934d3f
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-11-21 20:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (46.01%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -2,8 +2,5 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         n1 = int(num1)
         n2 = int(num2)
-        res = n1*n2
-        r = str(res)
-        return r
-
-        
+        res = n1 * n2
+        return str(res)
