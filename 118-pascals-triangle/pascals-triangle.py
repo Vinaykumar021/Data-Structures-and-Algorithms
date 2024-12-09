975eed63490f26bb0851b45a7e09128cfa461c69
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-12-10 00:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (18.66%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -3,8 +3,8 @@ class Solution:
         res = [[1]]
         for i in range(numRows - 1):
             temp = [0] + res[-1] + [0]
-            row = []
+            rows = []
             for j in range(len(res[-1]) + 1):
-                row.append(temp[j] + temp[j + 1])
-            res.append(row)
+                rows.append(temp[j] + temp[j + 1])
+            res.append(rows)
         return res
