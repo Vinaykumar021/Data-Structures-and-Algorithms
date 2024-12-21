295b926e0e49ff1131945641a4a0f833558f0806
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-12-21 23:54:46 +0530

    Time: 24 ms (26.66%) | Memory: 19 MB (13.31%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,7 +1,7 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = Counter(nums)
-        duplicate, missing = 0, 0
+        duplicate, missing = None, None
         for i in range(1, len(nums) + 1):
             if n[i] == 2:
                 duplicate = i
