d2f297fa03876c02be8c629942abe7ea6650cb1d
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2024-12-26 22:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (11.66%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,9 +5,9 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow_pointer = head
-        fast_pointer = head
-        while fast_pointer is not None and fast_pointer.next is not None:
-            slow_pointer = slow_pointer.next
-            fast_pointer = fast_pointer.next.next
-        return slow_pointer
+        cur = head
+        arr = []
+        while cur:
+            arr.append(cur)
+            cur = cur.next
+        return arr[len(arr)//2]
