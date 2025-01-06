953657e0cba60b7fb98b53098ccf0a7633d47db5
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2025-01-07 00:51:09 +0530

    Time: 7 ms (68.22%) | Memory: 19.3 MB (18.41%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.py
+++ 324-wiggle-sort-ii/wiggle-sort-ii.py
@@ -3,8 +3,5 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
         nums.sort(reverse = True)
-        nums[1::2], nums[::2] = nums[ : n//2], nums[n//2 : ]
-
-        
+        nums[1 : : 2], nums[ : : 2] = nums[ : len(nums)//2], nums[len(nums)//2 : ]
