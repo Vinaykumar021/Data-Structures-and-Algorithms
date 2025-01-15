6c8c1109a9b2ff5d2df53e8634794eb444025bf3
Vinaykumar021 <112115176+Vinaykumar021@users.noreply.github.com>
2025-01-15 21:11:39 +0530

    Time: 28 ms (14.48%) | Memory: 20 MB (7.51%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -5,9 +5,8 @@ class Solution:
         count = 0
         i, j = 0, 0
         while i < len(g) and j < len(s):
-            if g[i] <= s[j]:
+            if s[j] >= g[i]:
                 count += 1
                 i += 1
             j += 1
         return count
-            
